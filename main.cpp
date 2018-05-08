3d670550ce17566a5e90f6c1c761892f0546f4ac
NikitaFeodonit <nfeodonit@yandex.com>
2018-05-08 14:55:39 +0300

    Fill containers with random values and its printing.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,50 @@
  *    along with this program. If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <random>
+#include <utility>
+#include <vector>
+
 int main()
 {
+  constexpr int min = 1;
+  constexpr int max = 9;
+  constexpr int size = 50;
+
+  std::vector<int> testVector(size);
+  std::map<int, int> testMap;
+
+  // https://stackoverflow.com/a/19728404
+  std::random_device rd;
+  std::mt19937 rng(rd());
+  std::uniform_int_distribution<int> uni(min, max);
+  auto randomIntFunc = [&rng, &uni]() -> int { return uni(rng); };
+  auto randomPairFunc = [&rng, &uni, &testMap]() -> std::pair<int, int> {
+    return std::make_pair(testMap.size(), uni(rng));
+  };
+
+  std::generate(testVector.begin(), testVector.end(), randomIntFunc);
+  std::generate_n(
+      std::inserter(testMap, testMap.begin()), size, randomPairFunc);
+
+  std::cout << "Initial state:\n";
+
+  std::cout << "testVector: ";
+  for(auto iv : testVector) {
+    std::cout << iv << " ";
+  }
+  std::cout << "\n";
+
+  auto printMap = [](const std::pair<int, int>& element) {
+    std::cout << "[" << element.first << "]==" << element.second << " ";
+  };
+  std::cout << "testMap: ";
+  std::for_each(testMap.begin(), testMap.end(), printMap);
+  std::cout << "\n";
+
   return 0;
 }
